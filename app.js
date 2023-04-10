2e715ea8b0dafa253f26f9089214953699744885
Parth_Ghatge <parthghatge982@gmail.com>
2023-04-10 20:15:50 +0530

    Removed Redundant Files & Code

--- app.js
+++ app.js
@@ -1,35 +1,23 @@
-require('dotenv').config();
-require('express-async-errors');
+require("dotenv").config();
+require("express-async-errors");
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const mainRouter = require('./routes/main');
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const mainRouter = require("./routes/main");
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
-// middleware
-// To serve files in public folder at localhost/file_name
-app.use(express.static('./public'));
-// To parse incoming req body in json form to read it
+// In Built-middleware
+app.use(express.static("./public"));
 app.use(express.json());
 
-app.use('/api/v1',mainRouter);
+app.use("/api/v1", mainRouter);
 
-// Custom MDWs
+// Custom middleware
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 
-const start = async () => {
-  try {
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-start();
+app.listen(port, () => console.log(`Server is listening on port ${port}...`));
